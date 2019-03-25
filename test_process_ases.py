6c825ea5adcd995ffa55c9e359f8b19d6085860f
Florentin Rochet <florentin.rochet@uclouvain.be>
2019-03-25 11:07:07 +0100

    test ipv4 computation

--- test_process_ases.py
+++ test_process_ases.py
@@ -11,7 +11,13 @@ def test_compute_num_addresses():
     newAs = ASCust(asn, "be", asnInfo['data']['prefixes']['v4']['originating'], "2019-03-25")
     newAs.compute_num_ipv4_addresses()
     assert(newAs.num_ipv4_addresses == 995840) #source: https://bgp.he.net/AS2611
-    print(newAs.prefixes_originated)
+    asn = 16276
+    r = requests.get(RIS_PREFIX+str(asn)+"&query_time=2019-03-25&list_prefixes=true&types=o&af=v4&noise=filter")
+    asnInfo = r.json()
+    newAs = ASCust(asn, "fr", asnInfo['data']['prefixes']['v4']['originating'], "2019-03-25")
+    newAs.compute_num_ipv4_addresses()
+    assert(newAs.num_ipv4_addresses == 2962688) #source: https://bgp.he.net/AS16276
+    print("Test Ok.")
 
 
 if __name__ == "__main__":
