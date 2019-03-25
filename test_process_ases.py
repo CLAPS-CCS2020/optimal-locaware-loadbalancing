c5e4650738ddb4eb5975179b07c1fcbc44b7235b
Florentin Rochet <florentin.rochet@uclouvain.be>
2019-03-25 11:01:27 +0100

    add test

---
+++ test_process_ases.py
@@ -0,0 +1,20 @@
+import requests
+from slim_ases import *
+
+
+RIS_PREFIX = "https://stat.ripe.net/data/ris-prefixes/data.json?resource="
+
+def test_compute_num_addresses():
+    asn = 2611
+    r = requests.get(RIS_PREFIX+str(asn)+"&query_time=2019-03-25&list_prefixes=true&types=o&af=v4&noise=filter")
+    asnInfo = r.json()
+    newAs = ASCust(asn, "be", asnInfo['data']['prefixes']['v4']['originating'], "2019-03-25")
+    newAs.compute_num_ipv4_addresses()
+    assert(newAs.num_ipv4_addresses == 995840) #source: https://bgp.he.net/AS2611
+    print(newAs.prefixes_originated)
+
+
+if __name__ == "__main__":
+    test_compute_num_addresses()
+
+
