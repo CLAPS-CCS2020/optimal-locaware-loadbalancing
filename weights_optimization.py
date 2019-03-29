53054c63335b68f2be4607b3b66bfc0e58b4f052
Florentin Rochet <florentin.rochet@uclouvain.be>
2019-03-29 15:47:10 +0100

    add possibilities to generated subscaled problems of n ASes and m guards

--- weights_optimization.py
+++ weights_optimization.py
@@ -26,6 +26,8 @@ parser = argparse.ArgumentParser(description="")
 parser.add_argument("--tor_users_to_country", help="path to the pickle file containing the distribution of Tor users per country")
 parser.add_argument("--cust_ases", help="path to the pickle file containing the distribution of IPs per customer AS")
 parser.add_argument("--obj_function", type=int, help="Choice of objective function")
+parser.add_argument("--reduced_as_to", type=int, help="for test purpose, gives the number of ASes to keep")
+parser.add_argument("--reduced_guards_to", type=int, help="for test purpose, gives the number of guards to keep")
 parser.add_argument("--load_problem", help="filepth with problem to solve if already computed")
 parser.add_argument("--out_dir", help="out dir to save the .lp file")
 ## Note: simple at first. Later we may try to solve the problem for following network states and initialize variables of the n+1 state with
@@ -33,7 +35,7 @@ parser.add_argument("--out_dir", help="out dir to save the .lp file")
 parser.add_argument("--network_state", help="filepath to the network state containing Tor network's data")
 
 
-def load_and_compute_W(tor_users_to_country_file, cust_ases_file):
+def load_and_compute_W(tor_users_to_country_file, cust_ases_file, reduced_as_to=None):
     with open(tor_users_to_country_file, 'rb') as f:
         tor_users_to_country = pickle.load(f)
     with open(cust_ases_file, 'rb') as f:
@@ -44,22 +46,33 @@ def load_and_compute_W(tor_users_to_country_file, cust_ases_file):
     for countrycode in tor_users_to_country:
         # get the specific list of all Ases for this country
         #Improve it to get time too0000
-        r = requests.get(GETAS_URL+countrycode+"&lod=1")
-        if r.status_code == 200:
-            allAses = r.json()
-            for asn in allAses['data']['countries'][0]['routed']:
-                if asn in cust_ases:
-                    tor_users_per_as[asn] = tor_users_to_country[countrycode]*cust_ases[asn].num_ipv4_addresses
-                else:
-                    print("AS{} from country {} not in our dataset".format(asn, countrycode))
+        #r = requests.get(GETAS_URL+countrycode+"&lod=1")
+        #if r.status_code == 200:
+            #allAses = r.json()
+            #for asn in allAses['data']['countries'][0]['routed']:
+        for asn in cust_ases:
+                #if asn in cust_ases:
+            tor_users_per_as[asn] = tor_users_to_country[countrycode]*cust_ases[asn].num_ipv4_addresses
+                #else:
+                    #print("AS{} from country {} not in our dataset".format(asn, countrycode))
 
-        else:
-            raise ValueError("Something wrong happened: {}", r.json())
+        #else:
+            #raise ValueError("Something wrong happened: {}", r.json())
+    W = {}
+    if reduced_as_to:
+        allAses = list(tor_users_per_as.keys())
+        to_delete = len(allAses) - reduced_as_to
+        print("deleting {} elemets".format(to_delete))
+        for asn in allAses:
+            del tor_users_per_as[asn]
+            to_delete -= 1
+            if to_delete == 0:
+                break
     # Normalize
     tot = sum(tor_users_per_as.values())
-    W = {}
     for asn, value in tor_users_per_as.items():
         W[asn] = value/tot
+    pdb.set_trace()
     return W
 
 def build_fake_vuln_profile(guards, W):
@@ -75,7 +88,8 @@ def build_fake_vuln_profile(guards, W):
         if guard not in Vuln:
             Vuln[guard] = {}
         for asn in W:
-            Vuln[guard][asn] = random.randint(0,1000)
+            #Vuln[guard][asn] = random.randint(0,1000)
+            Vuln[guard][asn] = 1000 #uniform vulnerability
         tot += sum(Vuln[guard].values())
     for guard in guards:
         for asn in W:
@@ -83,10 +97,12 @@ def build_fake_vuln_profile(guards, W):
     return Vuln
 
 
-def modelize_opt_problem(W, ns_file, obj_function, out_dir=None):
+def modelize_opt_problem(W, ns_file, obj_function, out_dir=None, reduced_as_to=None, reduced_guards_to=None):
     network_state = get_network_state(ns_file)
     guardsfp = [relay for relay in network_state.cons_rel_stats if Flag.GUARD in network_state.cons_rel_stats[relay].flags and
             not Flag.EXIT in network_state.cons_rel_stats[relay].flags]
+    if reduced_guards_to:
+        guardsfp = guardsfp[0:reduced_guards_to]
     R = {}
     #Compute total G bandwidth
     G = 0
@@ -120,10 +136,10 @@ def modelize_opt_problem(W, ns_file, obj_function, out_dir=None):
     # one non-negative real number on the guard with the highest consensus weight
     objective = LpVariable("L_upper_bound", lowBound = 0, upBound=max_cons_weight)
     # Compute L as affine expressions involving LpVariables
-    print("Computng Affine Expressions for L, i.e., \sum W_iR_i")
+    print("Computing Affine Expressions for L, i.e., \sum W_iR_i")
     L = {}
     for guard in guardsfp:
-        L[guard] = LpAffineExpression([(R[asn][guard], W[asn]) for asn in W])
+        L[guard] = LpAffineExpression([(R[asn][guard], W[asn]) for asn in W], name="L({})".format(guard))
     print("Done.")
     ##  min_R max_j ( [\sum_{j} L(i)*Vuln(i)(j)  for i in all guards])
     if obj_function == 1:
@@ -153,7 +169,7 @@ def modelize_opt_problem(W, ns_file, obj_function, out_dir=None):
     ##   min_R (\sum_i \sum_j W(j)*R(i,j)*Vuln(i,j))
     elif obj_function == 3:
         print("Computing the lpSum of LpAffineExpressions as an objective function... (this can take time)")
-        location_aware += lpSum([LpAffineExpression([(W[asn]*Vuln[guard][asn], R[asn][guard]) for asn in W]) for guard in guardsfp]), "Z"
+        location_aware += lpSum([LpAffineExpression([(R[asn][guard], W[asn]*Vuln[guard][asn]) for asn in W]) for guard in guardsfp]), "Z"
     ##   min max_j (\sum_i R(i,j)*Vuln(i,j)) 
     elif obj_function == 4:
         location_aware += objective, "Z" #set objective function
@@ -174,10 +190,21 @@ def modelize_opt_problem(W, ns_file, obj_function, out_dir=None):
     print("Done. Writting out .lp file")
 
     ## Missing constraint for theta-GP-Secure TODO
+        
+    #Temporally wating for theta-GP-secure stuffs
+    #No relay gets more than 2 times its original selection probability
+    print("No relay gets more than 2 times its original selection probability:")
+    for asn in W:
+        for guard in guardsfp:
+            location_aware += R[asn][guard] <= 2*network_state.cons_rel_stats[guard].consweight
+
 
     # Write problem out:
     if out_dir:
-        outpath = os.path.join(out_dir, "location_aware_with_obj_{}.pickle".format(obj_function))
+        if reduced_as_to or reduced_guards_to:
+            outpath = os.path.join(out_dir, "location_aware_with_obj_{}_reducedas_{}_reducedguard_{}.pickle".format(obj_function, reduced_as_to,reduced_guards_to))
+        else:
+            outpath = os.path.join(out_dir, "location_aware_with_obj_{}.pickle".format(obj_function))
     else:
         outpath = "location_aware.pickle"
     #location_aware.writeLP(outpath)
@@ -190,11 +217,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     ## Compute appropritate values and modelize the optimization problem
     if args.tor_users_to_country and args.cust_ases:
-        W = load_and_compute_W(args.tor_users_to_country, args.cust_ases)
-        modelize_opt_problem(W, args.network_state, args.obj_function, args.out_dir)
+        W = load_and_compute_W(args.tor_users_to_country, args.cust_ases, args.reduced_as_to)
+        modelize_opt_problem(W, args.network_state, args.obj_function, args.out_dir, args.reduced_as_to, args.reduced_guards_to)
     ## Load the problem and solve() it
     elif args.load_problem:
         with open(args.load_problem, "rb") as f:
             location_aware = pickle.load(f)
             location_aware.solve(pulp.PULP_CBC_CMD(msg=1, threads=24))
             pdb.set_trace()
+            for v in location_aware.variables():
+                print(v.name, "=", v.varValue)
